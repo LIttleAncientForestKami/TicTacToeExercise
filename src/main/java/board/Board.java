2696bfd388c7c086d379eccc658ef81ce97517cf
KPP13 <kamilpoprawa@vp.pl>
2016-06-26 16:10:36 +0200

    Added inner helper class to switch marks. Now, it is possible to make some moves.

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -8,6 +8,8 @@ public class Board {
     private Mark currentMark;
     private final Set<Field> fieldSet;
 
+    private final MarkSwitcher markSwitcher;
+
     // factory method to generate initial empty fields
     private static Set<Field> generateEmptyFields(Integer size) {
         Set<Field> tmpSet = new TreeSet<>();
@@ -19,6 +21,7 @@ public class Board {
 
     Board() {
         fieldSet = Board.generateEmptyFields(N);
+        markSwitcher = new MarkSwitcher();
 
         // TODO: change when Player will be implemented...
         currentMark = Mark.O;
@@ -35,7 +38,10 @@ public class Board {
         return sb.toString();
     }
 
-    // chenges field from null mark to currentMark(O/X)
+
+
+    // changes field from null mark to currentMark(O/X)
+    // it is not possible to make move on existing mark (i.e. by accident)
     private boolean changeField(Integer nPos) {
         for (Field field : fieldSet) {
             if (field.equals( new Field( nPos, null ) )) {
@@ -47,17 +53,31 @@ public class Board {
         return false;
     }
 
-    // first mark on the board!
-    void putFirstMark() {
-        this.changeField( 5 );
+
+
+
+    void makeAMove(Integer pos) {
+        this.changeField(pos);
+        markSwitcher.switchMark();
     }
-
-
 
     // for test purposes
     public static void main(String[] args) {
         Board b = new Board();
-        b.putFirstMark();
+        b.makeAMove(1);
+        b.makeAMove(2);
+        b.makeAMove(3);
+        b.makeAMove(3);
+
         System.out.println( b );
+    }
+
+
+
+    // switch mark - helper class
+    private final class MarkSwitcher {
+        void switchMark() {
+            currentMark = currentMark.changeMark();
+        }
     }
 }

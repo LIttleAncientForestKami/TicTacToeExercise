0ae85cca2684f680317a6000b4ebbea48694c41f
KPP13 <kamilpoprawa@vp.pl>
2016-06-26 14:52:55 +0200

    Added representation of position. Position contains only one coordinate to make a code simple. HashCode, equals, comapreTo methods overriden to allow to compare Position objects.

---
+++ src/main/java/board/Position.java
@@ -0,0 +1,35 @@
+package board;
+
+// represents simple coordinate (position)
+public class Position implements Comparable<Position> {
+    // from 1 to 9
+    private final Integer position;
+
+    Position(Integer nPos) {
+        position = nPos;
+    }
+
+    @Override
+    public int compareTo(Position pos) {
+        return position.compareTo( pos.position );
+    }
+
+    @Override
+    public boolean equals(Object pos) {
+        if (this == pos) return true;
+        if (pos == null) return false;
+        if ( !(this.getClass().equals( pos.getClass() )) ) return false;
+        Position tmp = (Position) pos;
+        return position.equals( tmp.position );
+    }
+
+    @Override
+    public int hashCode() {
+        return position.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return position.toString();
+    }
+}
---
+++ src/test/java/board/PositionTest.java
@@ -0,0 +1,77 @@
+package board;
+
+import board.Position;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Test()
+public class PositionTest {
+    private List<Position> list = new LinkedList<>();
+
+    @BeforeClass
+    private void initialize() {
+        list.add( new Position(5) );
+        list.add( new Position(5) );
+        list.add( new Position(4) );
+        list.add( new Position(1) );
+        list.add( new Position(4) );
+    }
+
+    @DataProvider(name = "hash_eq")
+    private Object[][] provideObjectsForHashAndEqualityTest() {
+        return new Object[][] {
+                {true, list.get(0), list.get(0)},
+                {true, list.get(1), list.get(1)},
+                {true, list.get(2), list.get(2)},
+                {true, list.get(3), list.get(3)},
+                {true, list.get(4), list.get(4)},
+                {true, list.get(0), list.get(1)},
+                {false, list.get(0), list.get(2)},
+                {false, list.get(0), list.get(3)},
+                {false, list.get(0), list.get(4)},
+                {false, list.get(1), list.get(2)},
+                {false, list.get(1), list.get(3)},
+                {false, list.get(1), list.get(4)},
+                {false, list.get(2), list.get(3)},
+                {true, list.get(2), list.get(4)},
+                {false, list.get(3), list.get(4)}
+        };
+    }
+
+    @DataProvider(name = "compare")
+    private Object[][] provideCompareData() {
+        return new Object[][] {
+                {list.get(0).compareTo( list.get(1) ) == 0},
+                {list.get(0).compareTo( list.get(2) ) > 0},
+                {list.get(0).compareTo( list.get(3) ) > 0},
+                {list.get(0).compareTo( list.get(4) ) > 0},
+                {list.get(1).compareTo( list.get(2) ) > 0},
+                {list.get(1).compareTo( list.get(3) ) > 0},
+                {list.get(1).compareTo( list.get(4) ) > 0},
+                {list.get(2).compareTo( list.get(3) ) > 0},
+                {list.get(2).compareTo( list.get(4) ) == 0},
+                {list.get(3).compareTo( list.get(4) ) < 0}
+        };
+    }
+
+    @Test(dataProvider = "compare")
+    public void testGreater(boolean condition) {
+        Assert.assertTrue(condition);
+    }
+
+    @Test(dataProvider = "hash_eq")
+    public void testHashCode(boolean excepted, Position actualLeft, Position actualRight) {
+        Assert.assertEquals(actualLeft.hashCode() == actualRight.hashCode(), excepted);
+    }
+
+    @Test(dataProvider = "hash_eq")
+    public void testEquals(boolean excepted, Position actual1, Position actual2) {
+        Assert.assertEquals(actual1.equals( actual2 ), excepted);
+        Assert.assertEquals(actual2.equals( actual1 ), excepted);
+    }
+}

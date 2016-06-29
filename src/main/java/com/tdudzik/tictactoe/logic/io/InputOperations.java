65fd79ec3c24f85498234c49b32c150b1f8c59a2
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-06-29 16:34:54 +0200

    Create interface for input operations. It will make testing easier.

---
+++ src/main/java/com/tdudzik/tictactoe/logic/io/InputOperations.java
@@ -0,0 +1,9 @@
+package com.tdudzik.tictactoe.logic.io;
+
+import com.tdudzik.tictactoe.logic.board.Position;
+
+public interface InputOperations {
+
+    Position readPosition(String playerName);
+
+}

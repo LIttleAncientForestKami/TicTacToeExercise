4c00930db0b796f2cc624ce61868a6723504ef52
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-06-30 09:57:40 +0200

    Add methods to OutputOperations that are responsible for game that was won or ended in draw.

--- src/main/java/com/tdudzik/tictactoe/logic/io/OutputOperations.java
+++ src/main/java/com/tdudzik/tictactoe/logic/io/OutputOperations.java
@@ -1,9 +1,14 @@
 package com.tdudzik.tictactoe.logic.io;
 
 import com.tdudzik.tictactoe.logic.board.Board;
+import com.tdudzik.tictactoe.logic.player.Player;
 
 public interface OutputOperations {
 
     void drawBoard(Board board);
+
+    void gameWasWon(Board board, Player winner);
+
+    void gameEndedInDraw(Board board);
 
 }

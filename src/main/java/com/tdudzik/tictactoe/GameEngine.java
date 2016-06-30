22005cdcb4760d58e39307003f8fcac8c4db4706
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-06-30 10:06:43 +0200

    Scenario 3 finished. Without tests because I'm so tired of doing this game...

--- src/main/java/com/tdudzik/tictactoe/GameEngine.java
+++ src/main/java/com/tdudzik/tictactoe/GameEngine.java
@@ -2,39 +2,65 @@ package com.tdudzik.tictactoe;
 
 import com.tdudzik.tictactoe.logic.board.Board;
 import com.tdudzik.tictactoe.logic.board.BoardSize;
+import com.tdudzik.tictactoe.logic.board.Mark;
+import com.tdudzik.tictactoe.logic.game.Game;
+import com.tdudzik.tictactoe.logic.game.GameRules;
+import com.tdudzik.tictactoe.logic.game.WinningSequenceLength;
 import com.tdudzik.tictactoe.logic.io.InputHandler;
+import com.tdudzik.tictactoe.logic.io.InputOperations;
 import com.tdudzik.tictactoe.logic.io.OutputOperations;
 import com.tdudzik.tictactoe.logic.player.HumanPlayer;
 import com.tdudzik.tictactoe.logic.player.Player;
 import com.tdudzik.tictactoe.ui.ConsoleInputOperations;
 
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class GameEngine {
 
+    private final InputOperations inputOperations;
     private final OutputOperations outputOperations;
 
     private final AtomicInteger integer = new AtomicInteger(0);
 
-    public GameEngine(OutputOperations outputOperations) {
+    public GameEngine(InputOperations inputOperations, OutputOperations outputOperations) {
+        this.inputOperations = inputOperations;
         this.outputOperations = outputOperations;
     }
 
+//    public void launch() {
+//        Board board = Board.from(BoardSize.of(3));
+//
+//        Player player1 = new HumanPlayer(UUID.randomUUID().toString(), "Player 1");
+//        Player player2 = new HumanPlayer(UUID.randomUUID().toString(), "Player 2");
+//
+//        InputHandler inputHandler = new InputHandler(new ConsoleInputOperations(new Scanner(System.in)));
+//
+//        player1.playAsX(inputHandler, board);
+//        player2.playAsO(inputHandler, board);
+//        player1.playAsX(inputHandler, board);
+//
+//        outputOperations.drawBoard(board);
+//    }
     public void launch() {
-        Board board = Board.from(BoardSize.of(3));
+        GameRules gameRules = new GameRules(BoardSize.of(3), WinningSequenceLength.of(3));
+        Board board = Board.from(gameRules.getBoardSize());
 
-        Player player1 = new HumanPlayer(UUID.randomUUID().toString(), "Player 1");
-        Player player2 = new HumanPlayer(UUID.randomUUID().toString(), "Player 2");
+        Player player1 = inputOperations.readPlayer1();
+        Player player2 = inputOperations.readPlayer2();
 
-        InputHandler inputHandler = new InputHandler(new ConsoleInputOperations(new Scanner(System.in)));
+        Game game = new Game(gameRules, board, player1, Mark.X, player2, new Game.GameResult(false, Optional.empty()));
+        while (!game.isOver()) {
+            game = game.playTurn(new InputHandler(inputOperations), outputOperations);
+        }
 
-        player1.playAsX(inputHandler, board);
-        player2.playAsO(inputHandler, board);
-        player1.playAsX(inputHandler, board);
-
-        outputOperations.drawBoard(board);
+        if (game.wasWon()) {
+            outputOperations.gameWasWon(board, game.getWinner());
+        } else {
+            outputOperations.gameEndedInDraw(board);
+        }
     }
 
 }

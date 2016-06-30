723c431b6a126279ae995115b4fb2c3ce2f7e284
Maciej Kocur <maciej_kocur@epam.com>
2016-06-30 13:31:27 +0200

    One parameterized for applying any mark at any field
    Create one parameterized instead of 18 similar

--- src/test/java/com/maciejkocur/tictactoe/Board/SimpleMapBoardTest.java
+++ src/test/java/com/maciejkocur/tictactoe/Board/SimpleMapBoardTest.java
@@ -2,31 +2,35 @@ package com.maciejkocur.tictactoe.Board;
 
 import com.maciejkocur.tictactoe.Board.impl.SimpleMapBoard;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleMapBoardTest {
-
-    @Test
-    public void appliesMark0AtCenter() {
-        // given
-        Board board = createBoard();
-        // when
-        board.appliesMark(5, Mark.CIRCLE);
-        // then
-        Assert.assertEquals(Mark.CIRCLE, board.getMarkAtField(5));
+    @DataProvider(name = "fields")
+    public static Object[][] fields() {
+        Object[][] fields = new Object[18][2];
+        for (int i = 0; i < 18; i++) {
+            fields[i][0] = i + 1;
+            fields[i][1] = Mark.CIRCLE;
+        }
+        for (int i = 0; i < 9; i++) {
+            fields[i + 9][0] = i + 1;
+            fields[i + 9][1] = Mark.CROSS;
+        }
+        return fields;
     }
 
-    @Test
-    public void appliesMarkXAtCenter() {
-        // given
+    @Test(dataProvider = "fields")
+    public void appliesAnyMoveAtAnyEmptyField(Integer field, Mark mark) {
+        //given
         Board board = createBoard();
-        // when
-        board.appliesMark(5, Mark.CROSS);
-        // then
-        Assert.assertEquals(Mark.CROSS, board.getMarkAtField(5));
+        //when
+        board.appliesMark(field, mark);
+        //then
+        Assert.assertEquals(mark, board.getMarkAtField(field));
     }
 
     private Board createBoard() {

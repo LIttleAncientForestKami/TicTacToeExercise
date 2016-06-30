2caa9892d7d1099d8080c1e3cd90dd370f0eeb09
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-06-30 10:04:06 +0200

    Implement missing methods.

--- src/main/java/com/tdudzik/tictactoe/ui/ConsoleInputOperations.java
+++ src/main/java/com/tdudzik/tictactoe/ui/ConsoleInputOperations.java
@@ -2,15 +2,37 @@ package com.tdudzik.tictactoe.ui;
 
 import com.tdudzik.tictactoe.logic.board.Position;
 import com.tdudzik.tictactoe.logic.io.InputOperations;
+import com.tdudzik.tictactoe.logic.player.HumanPlayer;
+import com.tdudzik.tictactoe.logic.player.Player;
+import com.tdudzik.tictactoe.logic.player.PlayerFactory;
 
 import java.util.Scanner;
+import java.util.UUID;
 
 public class ConsoleInputOperations implements InputOperations {
 
     private final Scanner scanner;
+    private final PlayerFactory playerFactory;
 
-    public ConsoleInputOperations(Scanner scanner) {
-        this.scanner = new Scanner(System.in);
+    public ConsoleInputOperations(Scanner scanner, PlayerFactory playerFactory) {
+        this.scanner = scanner;
+        this.playerFactory = playerFactory;
+    }
+
+    @Override
+    public Player readPlayer1() {
+        System.out.print("Player 1: ");
+        String playerName = scanner.nextLine();
+
+        return playerFactory.createHumanPlayer(playerName);
+    }
+
+    @Override
+    public Player readPlayer2() {
+        System.out.print("Player 2: ");
+        String playerName = scanner.nextLine();
+
+        return playerFactory.createHumanPlayer(playerName);
     }
 
     @Override

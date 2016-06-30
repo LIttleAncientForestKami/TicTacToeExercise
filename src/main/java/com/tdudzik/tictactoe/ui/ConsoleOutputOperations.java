a014e6aaf2e075e65b149af57e091812ba04af88
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-06-30 09:31:21 +0200

    Add implementation of gameWasWon and gameEndedInDraw methods in ConsoleOutputOperations.

--- src/main/java/com/tdudzik/tictactoe/ui/ConsoleOutputOperations.java
+++ src/main/java/com/tdudzik/tictactoe/ui/ConsoleOutputOperations.java
@@ -4,12 +4,15 @@ import com.tdudzik.tictactoe.logic.board.Board;
 import com.tdudzik.tictactoe.logic.board.Mark;
 import com.tdudzik.tictactoe.logic.board.Position;
 import com.tdudzik.tictactoe.logic.io.OutputOperations;
+import com.tdudzik.tictactoe.logic.player.Player;
 
 public class ConsoleOutputOperations implements OutputOperations {
 
     @Override
     public void drawBoard(Board board) {
         Position currentPosition = Position.of(1);
+
+        System.out.println();
         for (int i = 0; i < board.getSize().getValue(); i++) {
             System.out.print(" | ");
             for (int j = 0; j < board.getSize().getValue(); j++) {
@@ -24,6 +27,19 @@ public class ConsoleOutputOperations implements OutputOperations {
             }
             System.out.println();
         }
+        System.out.println();
+    }
+
+    @Override
+    public void gameWasWon(Board board, Player winner) {
+        drawBoard(board);
+        System.out.println("The winner is: " + winner.getName() + ". :)");
+    }
+
+    @Override
+    public void gameEndedInDraw(Board board) {
+        drawBoard(board);
+        System.out.println("There is no winner. :(");
     }
 
 }

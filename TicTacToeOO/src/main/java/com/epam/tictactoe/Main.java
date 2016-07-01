3332afe678c203f1b7a368b63e375c4b6c13efbb
Grzegorz Sledz <grzegorz_sledz@epam.com>
2016-07-01 14:47:33 +0200

    TicTacToeGame was added. It contains game logic for scenario3. Scenario 3 allow as to play game without checking who is the winner"

--- TicTacToeOO/src/main/java/com/epam/tictactoe/Main.java
+++ TicTacToeOO/src/main/java/com/epam/tictactoe/Main.java
@@ -10,6 +10,6 @@ public class Main {
 
     public static void main(String[] args){
         TicTacToeGame game=new TicTacToeGame(new Dimension(3));
-        game.play();
+        game.playO().playX().playO().playX().playO().playX().playO().playX().playO();
     }
 }
--- TicTacToeOO/src/main/java/com/epam/tictactoe/controller/TicTacToeGame.java
+++ TicTacToeOO/src/main/java/com/epam/tictactoe/controller/TicTacToeGame.java
@@ -1,67 +1,66 @@
 package com.epam.tictactoe.controller;
 
-import com.epam.tictactoe.board.Board;
-import com.epam.tictactoe.board.IBoard;
-import com.epam.tictactoe.board.IDimension;
-import com.epam.tictactoe.board.Position;
+import com.epam.tictactoe.board.*;
+import com.epam.tictactoe.input.ConsoleInput;
+import com.epam.tictactoe.input.IInput;
 import com.epam.tictactoe.managers.IPlayerManager;
 import com.epam.tictactoe.managers.PlayerManager;
+import com.epam.tictactoe.player.IPlayer;
 
 /**
  * Created by grzegorz_sledz on 30.06.16.
  */
-public class TicTacToeGame{
+public class TicTacToeGame {
 
     private final IBoard board;
+    private final IDimension dimension;
     private final IPlayerManager playerManager;
+    private final IInput console;
+    private int correctTurns;
 
     public TicTacToeGame(IDimension dimension) {
         this.board = new Board(dimension);
-        this.playerManager=new PlayerManager();
+        this.dimension = dimension;
+        this.playerManager = new PlayerManager();
+        this.console = new ConsoleInput();
     }
 
-    public void play(){
-        board.placeMark(new Position(5), playerManager.current().sign());
-        board.placeMark(new Position(4), playerManager.next().sign());
-        board.placeMark(new Position(3), playerManager.next().sign());
-        board.placeMark(new Position(2), playerManager.next().sign());
+    public TicTacToeGame playO(){
+        makeTurn(playerManager.playerO());
+        return this;
+    }
+    public TicTacToeGame playX(){
+        makeTurn(playerManager.playerX());
+        return this;
+    }
+
+    private void makeTurn(IPlayer player){
+        doMove(player);
         System.out.println(board);
+        checkIsWinner(player);
     }
 
-//
-//    void start() {
-//        currentPlayer = playerO;
-//        IPosition position;
-//        int turns = 0;
-//        System.out.println(board);
-//        do {
-//            doMove();
-//            turns++;
-//            System.out.println(board);
-//            if (turns >= 5 && arbiter.isWinner(currentPlayer.winningSeqiences())) {
-//                System.out.println("The winner is: " + currentPlayer);
-//                stop();
-//            }
-//            nextPlayer();
-//        } while (!board.isFullyFilled());
-//        System.out.println("Nobody won");
-//    }
-//
-//    public void doMove() {
-//        boolean correctMove = false;
-//        do {
-//            System.out.println(currentPlayer + " choose field [1-" + dimension.getValue() * dimension.getValue() + "]: ");
-//            IPosition position = new Position(input.read());
-//            correctMove = board.placeMark(position, currentPlayer.getSign());
-//            if (correctMove) {
-//                currentPlayer.saveMove(position);
-//            } else {
-//                System.out.println(position + " is not valid. Try again!!");
-//            }
-//        } while (!correctMove);
-//    }
-//
-//    private void stop() {
-//        System.exit(0);
-//    }
+    private void checkIsWinner(IPlayer player ){
+        if(board.checkIsFullyFilled()){
+            System.out.println("Nobody won");
+        }
+    }
+
+    private void doMove(IPlayer player) {
+        boolean isCorrectMove = false;
+        IPosition toPosition;
+        do {
+            System.out.println(player + " choose field [1-" + dimension.value() * dimension.value() + "]: ");
+             toPosition = new Position(console.readInt());
+            isCorrectMove = board.placeMark(toPosition, player.sign());
+            if (!isCorrectMove) {
+                System.out.println(toPosition + " is not valid. Try again!!");
+            }
+        } while (!isCorrectMove);
+        player.saveTurn(toPosition);
+    }
+
+    private void stop() {
+        System.exit(0);
+    }
 }

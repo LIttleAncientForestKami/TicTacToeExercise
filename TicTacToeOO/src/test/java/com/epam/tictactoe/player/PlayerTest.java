726d597f1ef5671d6c5b68ff401f2fa0668185ab
Grzegorz Sledz <grzegorz_sledz@epam.com>
2016-07-01 14:46:35 +0200

    PlayerTest's was added

--- TicTacToeOO/src/test/java/com/epam/tictactoe/player/PlayerTest.java
+++ TicTacToeOO/src/test/java/com/epam/tictactoe/player/PlayerTest.java
@@ -1,5 +1,7 @@
 package com.epam.tictactoe.player;
 
+import com.epam.tictactoe.board.IPosition;
+import com.epam.tictactoe.board.Position;
 import com.epam.tictactoe.enums.Sign;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -25,6 +27,10 @@ public class PlayerTest {
     }
 
     @Test
+    public void equalsOfPlayerAndNullShouldBeFalse() {
+        Assert.assertFalse(new PlayerX().equals(null));
+    }
+    @Test
     public void toStringPlayerO() {
         Assert.assertTrue(new PlayerO().toString().equals("Player O"));
     }
@@ -32,6 +38,27 @@ public class PlayerTest {
     @Test
     public void toStringPlayerX() {
         Assert.assertTrue(new PlayerX().toString().equals("Player X"));
+    }
+
+    @Test
+    public void signPlayerOShouldBeSignO() {
+        Assert.assertEquals(new PlayerO().sign(),Sign.O);
+    }
+
+    @Test
+    public void signPlayerXShouldBeSignX() {
+        Assert.assertEquals(new PlayerX().sign(),Sign.X);
+    }
+    @Test
+    public void lastTurnAfterPlayerCreationShouldBeNull() {
+        IPlayer player=new PlayerX();
+        Assert.assertNull(player.lastTurn());
+    }
+    @Test
+    public void lastTurnAfterTurnToPostionFiveShouldBeFive() {
+        IPlayer player=new PlayerX();
+        player.saveTurn(new Position(5));
+        Assert.assertEquals(player.lastTurn(),new Position(5));
     }
 
 }
